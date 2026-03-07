16612dc4b44b5e0d6762ba8716ef5b9249655347
erpranto <erpranto55@gmail.com>
2026-03-07 21:13:11 +0600

    Main.html initialized and some function added

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById("loginBtn").addEventListener("click", function () {
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  if (username === "admin" && password === "admin123") {
+    window.location.href = "main.html";
+  } else {
+    alert("Invalid Credentials");
+  }
+});
