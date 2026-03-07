531cd1accde7a6ef60a72f501d3a1a233f7c6419
erpranto <erpranto55@gmail.com>
2026-03-07 23:51:30 +0600

    Priority colour added

--- issues.js
+++ issues.js
@@ -20,13 +20,24 @@ function displayIssues(issues) {
   issues.forEach((issue) => {
     let borderColor =
       issue.status === "open" ? "border-green-500" : "border-purple-500";
+
+    let priorityColor = "";
+    if (issue.priority === "high") {
+      priorityColor = "bg-red-100 text-red-500";
+    } else if (issue.priority === "medium") {
+      priorityColor = "bg-yellow-100 text-yellow-600";
+    } else {
+      priorityColor = "bg-gray-200 text-gray-500";
+    }
+
     const card = document.createElement("div");
     card.className = `bg-white rounded-lg shadow p-4 border-t-4 ${borderColor} cursor-pointer hover:shadow-lg transition`;
     card.innerHTML = `
-
 <div class="flex justify-between items-center mb-2">
 <span class="text-green-500 text-sm">●</span>
-<span class="badge badge-error badge-sm">${issue.priority}</span>
+<span class="px-3 py-1 text-xs rounded-full ${priorityColor}">
+${issue.priority.toUpperCase()}
+</span>
 </div>
 
 <h3 class="font-semibold text-sm mb-2">
@@ -53,7 +64,7 @@ ${issue.createdAt}
   });
 }
 
-function loadIssues(type) {
+function filterIssues(type) {
   if (type === "all") {
     displayIssues(allIssues);
   } else {
@@ -64,29 +75,20 @@ function loadIssues(type) {
 
 function openModal(issue) {
   document.getElementById("modalTitle").innerText = issue.title;
-
   document.getElementById("modalDescription").innerText = issue.description;
-
   document.getElementById("modalStatus").innerText = issue.status;
-
   document.getElementById("modalAuthor").innerText = issue.author;
-
   document.getElementById("modalPriority").innerText = issue.priority;
-
   document.getElementById("modalCreated").innerText = issue.createdAt;
-
   document.getElementById("issueModal").showModal();
 }
 
 async function searchIssues() {
   const text = document.getElementById("searchInput").value;
-
   const res = await fetch(
     `https://phi-lab-server.vercel.app/api/v1/lab/issues/search?q=${text}`,
   );
-
   const data = await res.json();
-
   displayIssues(data.data);
 }
 
